dc40d6ac42c6421e8d75044607591a3a3487766d
Mykel68 <olowookeremicheal9568@gmail.com>
2024-03-21 16:43:17 +0100

    report count tested

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -17,6 +17,7 @@ router.put("/update-report/:id", reportController.updateReport);
 // Delete report
 router.delete("/delete-report/:id", reportController.deleteReport);
 
-// router.post("/login", reportController.login);
+// get Report Count
+router.get("/getReportCount", reportController.getReportCount);
 
 module.exports = router;
